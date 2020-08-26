16836c1ad979283c379434d1afcc0fdac2e2c60a
Nick Chapsas <elfocrash@gmail.com>
2020-08-26 20:51:52 +0100

    Added a Cli project

---
+++ Servermore.Cli/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Servermore.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO commands
+
+            //1. dotnet servermore pack
+            //    - Build the project
+            //    - Pick the artifacts and the dependency dlls
+            //    - Package them up in a zip
+
+            //2. dotnet servermore deploy --local
+            //    - Will pack and paste the zip at the specified directory
+
+            //3. dotnet servermore deploy --remote
+            //    - Will pack and upload the zip at the specified Servermore server
+        }
+    }
+}
